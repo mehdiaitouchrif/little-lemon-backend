3e908ac5468fb46ba9d7f5ac23599ce590b50efa
Mehdi Ait Ouchrif <mehdi.aitouchrif@outlook.com>
2023-05-07 23:11:36 +0100

    set renderers, accept xml

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -126,6 +126,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_xml.renderers.XMLRenderer'
+    ],
+}
+
 INTERNAL_IPS = [
     '127.0.0.1'
 ]
