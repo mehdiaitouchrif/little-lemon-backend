215475643c146f4b70a89dd337d6c97a1bbab2e0
Mehdi Ait Ouchrif <mehdi.aitouchrif@outlook.com>
2023-05-31 02:09:40 +0100

    homepage docs with swagger

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'corsheaders',
     'littlelemonapi',
     'rest_framework.authtoken',
--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -18,12 +18,30 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-from rest_framework import routers
+
+from rest_framework import routers, permissions
 from littlelemonapi.views import MenuItemImageViewSet
 
 router = routers.DefaultRouter()
 router.register(r'menu-items/(?P<menu_item_pk>\d+)/images', MenuItemImageViewSet, basename='menu-item-images')
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Welcome to Little Lemon API Documentation",
+        default_version='v1',
+        description="""The Little Lemon Backend API is a Django-powered backend that offers a range of endpoints to manage customers, menu items, categories, orders, delivery crew users, and user groups. 
+                       Users can register, login, and logout, while customers can browse the menu, add items to their cart, and place orders. Managers have full control over categories and menu items, can update orders, 
+                       assign delivery crew members, and manage customer groups. Delivery crew users can easily update their assigned orders. This API provides a robust foundation for efficient operations and a seamless user experience 
+                       within the Little Lemon app.""",
+        terms_of_service="https://www.example.com/policies/terms/",
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,7 +49,8 @@ urlpatterns = [
     path('api/', include('djoser.urls')),
     path('api/', include('djoser.urls.authtoken')),
     path('api/', include(router.urls)),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('__debug__/', include('debug_toolbar.urls'))
 ] 
 
 if settings.DEBUG:
